addd4efa4f164697a72315184b67208274f41f2f
clayknife <shikame-7@hotmail.com>
2020-04-04 22:35:16 +0300

    navbar styling

--- js/shared-style-script.js
+++ js/shared-style-script.js
@@ -3,10 +3,8 @@ $(document).ready(function(){
         var currentScrollPos = window.pageYOffset;
         if (currentScrollPos == 0) {
           document.getElementById("navbar").style.boxShadow = "";
-          document.getElementById("navbar").style.backgroundColor = "";
         } else {
-          document.getElementById("navbar").style.boxShadow = " 0 0px 8px 0 black";
-          document.getElementById("navbar").style.backgroundColor = " rgba(242,241,235,0.97)";
+          document.getElementById("navbar").style.boxShadow = "0 0px 8px 0 black";
         }
       }
 });
